ef875cab9114296494f80705f2283c17e6a27a1f
Jared Eklin <jaredeklin@gmail.com>
2020-07-29 19:46:30 -0600

    Add sort to countries dropdown list.

--- client/src/PlayerForm/CountriesList.jsx
+++ client/src/PlayerForm/CountriesList.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { COUNTRIES } from '../constants';
 
+const sortedByName = Object.keys(COUNTRIES)
+  .map((countryCode) => ({
+    countryCode,
+    name: COUNTRIES[countryCode],
+  }))
+  .sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+
 const CountriesList = () => (
   <>
     <option value="">--Please choose a country--</option>
-    {Object.keys(COUNTRIES).map((countryCode) => {
-      return (
-        <option key={countryCode} value={countryCode}>
-          {COUNTRIES[countryCode]}
-        </option>
-      );
-    })}
+    {sortedByName.map(({ countryCode, name }) => (
+      <option key={countryCode} value={countryCode}>
+        {name}
+      </option>
+    ))}
   </>
 );
 
